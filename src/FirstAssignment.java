fd6d3b256af402c6787fee04a55599f353c1d9ce
shadiaislamankur <shadiaislam469@gmail.com>
2024-05-03 22:06:36 +0600

    first commit

---
+++ src/FirstAssignment.java
@@ -0,0 +1,9 @@
+public class FirstAssignment {
+    public static void main(String[] args) {
+        System.out.println("id: 101,");
+        System.out.println("title: iphone17,");
+        System.out.println("price: 2000 euros,");
+        System.out.println("description: perfect product with best image quality,");
+        System.out.println("category: phone");
+    }
+}
